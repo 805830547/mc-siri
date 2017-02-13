005dcb956e3dfbba176c1d53d3104bd6a47309bb
callmewhy <whywanghai@gmail.com>
2017-02-13 23:30:38 +0800

    add aiff

--- say.js
+++ say.js
@@ -2,16 +2,19 @@ const fs = require('fs')
 const spawnSync = require('child_process').spawnSync
 const lyric = fs.readFileSync('./lyric.txt').toString()
 
+function padLeft(text, char, number) {
+    var result = '' + text
+    while (result.length < number) {
+        result = char + result
+    }
+    return result
+}
+
 const words = {}
 
 lyric.split('\n')
     .filter(line => line.length > 0)
-    .map((line, i) => {
-        const lineN = i
-        line.replace(/ /g, '')
-            .split('')
-            .forEach((word, i) => {
-                const filename = lineN + '-' + i + '(' + word + ').aiff'
-                spawnSync('say', [word, '-o', './aiff/' + filename])
-            })
+    .map((line, lineN) => {
+        const filename = padLeft(lineN, '0', 3) + '-' + line + '.aiff'
+        spawnSync('say', [line, '-o', './aiff/' + filename])
     })
